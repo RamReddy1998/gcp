5b3957356a41991fe56192dc1e7ee397d4b49b2e
Jangamgari Ram Reddy <109741542+RamReddy1998@users.noreply.github.com>
2026-04-17 16:36:33 +0530

    Remove Express server setup and add hello world log

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 8080; // Cloud Run expects this
-
-app.get('/', (req, res) => {
-  res.send('Hello from Cloud Run! 🚀');
-});
-
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+console.log("hello world")
